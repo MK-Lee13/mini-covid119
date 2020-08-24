7d7f058ce6046b4869bc42efb7b0a28bc0feb50e
koreaaerospaceU <zx811@naver.com>
2020-08-25 01:10:39 +0900

    TODO: 크롤러 제작 및 DB 구축

--- backend/routes/api/covid119/index.js
+++ backend/routes/api/covid119/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 const controller = require('./covid119.controller')
 
 // 나중에 알아서 라우트 패스 잘 나눠주세요
-router.post('/get', controller.get)
+router.get('/get', controller.get)
 
 module.exports = router
